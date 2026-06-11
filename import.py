d148cc50c327127c4e31f94e5d4c707be60a0abe
Diego Bellani <bellanidiego@gmail.com>
2026-06-11 12:48:32 +0200

    Aded LowerQNNOps

--- import.py
+++ import.py
@@ -162,8 +162,9 @@ for root, dirs, files in os.walk("3rdparty/tiny"):
                 else:
                     mixed_networks.append(model_path)
                 mod = tvm.relax.frontend.tflite.from_tflite(tflite_model)
-                mod = tvm.relax.transform.NormalizeQQDPatterns()(mod)
-                mod = tvm.relax.transform.RewriteQDQPatterns()(mod)
+                mod = tvm.relax.transform.NormalizeQDQPatterns()(mod)
+                mod = tvm.relax.transform.RewriteQDQPatternsToQNNOps()(mod)
+                mod = tvm.relax.transform.LowerQNNOps()(mod)
                 mod.show()
             except tvm.error.OpNotImplemented as ex:
                 errors.append(model_path)
